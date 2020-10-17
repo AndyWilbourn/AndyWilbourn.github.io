23113994cd9cc06f877dca8459e6630af9847ff8
AndyWilbourn <awilbourn.0@gmail.com>
2020-10-16 20:01:33 -0400

    Implemented working Quantity
    Got the quantity buttons working. Will need to save the number somewhere to carry across to the product page, and make price dynamic.

---
+++ assignment_6/productDetail.js
@@ -0,0 +1,16 @@
+function incrementUp()
+{
+    var value = parseInt(document.getElementById('number').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value++;
+    document.getElementById('number').value = value;
+}
+
+function incrementDown() {
+    var value = parseInt(document.getElementById('number').value, 10);
+    value = isNaN(value) ? 0 : value;
+    if (value > 1) {
+      value--;
+    }
+    document.getElementById('number').value = value;
+  }
